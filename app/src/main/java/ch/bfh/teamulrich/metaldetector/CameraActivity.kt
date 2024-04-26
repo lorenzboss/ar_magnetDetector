c9976119401e14291e5f9427d85b204b721ab1c9
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-04-26 22:18:21 +0200

    set result and extra for the intent to be sent to LogBook app

--- app/src/main/java/ch/bfh/teamulrich/metaldetector/CameraActivity.kt
+++ app/src/main/java/ch/bfh/teamulrich/metaldetector/CameraActivity.kt
@@ -1,6 +1,7 @@
 package ch.bfh.teamulrich.metaldetector
 
 import QRCameraView
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,11 +10,13 @@ class CameraActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QRCameraView(onQRCodeResult = { _, value ->
-                // TODO: set result and extra for the intent to be sent to LogBook app
+            QRCameraView(onQRCodeResult = { uri, value ->
+                setResult(RESULT_OK, Intent().apply {
+                    putExtra("image_uri", uri)
+                    putExtra("qr_code_value", value)
+                })
                 finish()
             })
         }
-
     }
 }
