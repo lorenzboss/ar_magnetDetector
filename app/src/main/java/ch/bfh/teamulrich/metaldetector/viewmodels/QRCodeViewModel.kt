7ebc59a0c085ed5c91390edcd52b9949ff9d3d5e
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-04-26 22:54:56 +0200

    moving the logic for sending the intent to the viewModel

--- app/src/main/java/ch/bfh/teamulrich/metaldetector/viewmodels/QRCodeViewModel.kt
+++ app/src/main/java/ch/bfh/teamulrich/metaldetector/viewmodels/QRCodeViewModel.kt
@@ -4,11 +4,11 @@ import android.app.Application
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
-import ch.bfh.teamulrich.metaldetector.R
 import org.json.JSONObject
 
 class QRCodeViewModel(application: Application) : AndroidViewModel(application) {
@@ -31,7 +31,18 @@ class QRCodeViewModel(application: Application) : AndroidViewModel(application) 
         this.qrCode = ""
     }
 
-    fun sendQRCodeToLogApp() {
-        // send Intent to LogBook app with task ID and solution value
+    fun sendQRCodeToLogApp(context: Context) {
+        val log = JSONObject()
+        val intent: Intent?
+        log.put("task", "Metalldetektor")
+        log.put("solution", qrCode)
+        intent = Intent("ch.apprun.intent.LOG")
+        intent.putExtra("ch.apprun.logmessage", log.toString())
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        try {
+            context.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("LogBookError", " LogBook application is not installed on this device.")
+        }
     }
 }
--- app/src/main/java/ch/bfh/teamulrich/metaldetector/views/reader/QRCodeView.kt
+++ app/src/main/java/ch/bfh/teamulrich/metaldetector/views/reader/QRCodeView.kt
@@ -1,9 +1,6 @@
 package ch.bfh.teamulrich.metaldetector.views.reader
 
-import android.content.ActivityNotFoundException
 import android.content.Context
-import android.content.Intent
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.launch
 import androidx.compose.foundation.layout.Arrangement
@@ -27,7 +24,6 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ch.bfh.teamulrich.metaldetector.viewmodels.QRCodeViewModel
 import ch.bfh.teamulrich.metaldetector.viewmodels.ReceiveQRCode
-import org.json.JSONObject
 
 
 @Composable
@@ -61,18 +57,7 @@ fun QRCodeView(viewModel: QRCodeViewModel = viewModel()) {
                 Spacer(modifier = Modifier.height(24.dp))
                 Button(
                     onClick = {
-                        val log = JSONObject()
-                        val intent: Intent?
-                        log.put("task", "Metalldetektor")
-                        log.put("solution", code)
-                        intent = Intent("ch.apprun.intent.LOG")
-                        intent.putExtra("ch.apprun.logmessage", log.toString())
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        try {
-                            context.startActivity(intent)
-                        } catch (e: ActivityNotFoundException) {
-                            Log.e("Logger", " LogBook application is not installed on this device.")
-                        }
+                        viewModel.sendQRCodeToLogApp(context)
                     },
                     modifier = Modifier.width(164.dp)
                 ) {
