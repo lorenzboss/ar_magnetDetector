dc17176e9b49866d074c69b6598d85b3ec143f61
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-04-26 19:52:25 +0200

    obtain magnetic sensor reading and update state

--- app/src/main/java/ch/bfh/teamulrich/metaldetector/viewmodels/MetalDetectorViewModel.kt
+++ app/src/main/java/ch/bfh/teamulrich/metaldetector/viewmodels/MetalDetectorViewModel.kt
@@ -5,10 +5,9 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlin.math.max
 import kotlin.math.sqrt
 
@@ -17,8 +16,8 @@ data class MetalDetectorState(var currentMagnitude: Double = 1.0, var maxMagnitu
 class MetalDetectorViewModel(application: Application) : AndroidViewModel(application),
     SensorEventListener {
 
-    var state by mutableStateOf(MetalDetectorState())
-        private set
+    private val _state = MutableStateFlow(MetalDetectorState())
+    val state: StateFlow<MetalDetectorState> = _state
 
     private val sensorManager = application.getSystemService(SensorManager::class.java)
     private val magneticFieldSensor: Sensor? =
@@ -33,7 +32,13 @@ class MetalDetectorViewModel(application: Application) : AndroidViewModel(applic
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        // TODO: obtain magnetic sensor reading and update variable 'state'
+        val values = event?.values ?: return
+        val currentMagnitude = sqrt(
+            (values[0] * values[0] + values[1] * values[1] + values[2] * values[2]).toDouble()
+        )
+        val maxMagnitude = max(_state.value.maxMagnitude, _state.value.currentMagnitude)
+
+        _state.value = MetalDetectorState(currentMagnitude, maxMagnitude)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
